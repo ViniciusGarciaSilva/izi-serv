3ba962d8fd1d5280d801d0a7c8b375f235477a2c
Vinicius Garcia <vinicius.garcia.silva@usp.br>
2018-09-28 16:50:55 -0300

    Cards Idea, Project, Service

---
+++ src/components/cards/service-card.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import service from '../../images/service.svg'
+
+class ServiceCard extends Component {
+    render() {
+        return (
+            <Card style={{margin:30}}>
+                <Image src={service} />
+                <Card.Content>
+                    <Card.Header>Execute um serviço</Card.Header>
+                    <Card.Description>Escolha uma das vagas disponíveis de um projeto e ofereça seu serviço</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                </Card.Content>
+            </Card>
+        )
+    }
+}
+
+export default ServiceCard;
