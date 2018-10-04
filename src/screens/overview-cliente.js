2d61679d142a7910c7825fca738680a755a4de96
diogo-shiroma <diogoshiroma@hotmail.com>
2018-10-04 14:53:49 -0300

    removing unused div

--- src/screens/overview-cliente.js
+++ src/screens/overview-cliente.js
@@ -33,64 +33,56 @@ class OverviewCliente extends Component {
                         <Grid.Column width={14}>
                             <h1>SPRINTS</h1>
                             <Grid.Row style={{marginRight: 40}}>
-                                <Sidebar.Pusher>
-                                    <Segment raised>
-                                        <Header as='h3'>Sprint 1</Header>
-                                        <Checkbox label='Starting project' />
-                                        <Segment basic style={{textAlign: 'justify'}}>
-                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-                                            Aenean commodo ligula eget dolor. Aenean massa strong. 
-                                            Cum sociis natoque penatibus et magnis dis parturient montes, 
-                                            nascetur ridiculus mus. Donec quam felis, ultricies nec, 
-                                            pellentesque eu, pretium quis, sem. 
-                                            Nulla consequat massa quis enim. Donec pede justo, fringilla vel, 
-                                            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, 
-                                            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede 
-                                            link mollis pretium. Integer tincidunt. Cras dapibus. 
-                                            Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
-                                            Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. 
-                                            Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. 
-                                            Phasellus viverra nulla ut metus varius laoreet. 
-                                            Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. 
-                                            Curabitur ullamcorper ultricies nisi.
-                                        </Segment>
+                                <Segment raised>
+                                    <Header as='h3'>Sprint 1</Header>
+                                    <Checkbox label='Starting project' />
+                                    <Segment basic style={{textAlign: 'justify'}}>
+                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                                        Aenean commodo ligula eget dolor. Aenean massa strong. 
+                                        Cum sociis natoque penatibus et magnis dis parturient montes, 
+                                        nascetur ridiculus mus. Donec quam felis, ultricies nec, 
+                                        pellentesque eu, pretium quis, sem. 
+                                        Nulla consequat massa quis enim. Donec pede justo, fringilla vel, 
+                                        aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, 
+                                        imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede 
+                                        link mollis pretium. Integer tincidunt. Cras dapibus. 
+                                        Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
+                                        Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. 
+                                        Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. 
+                                        Phasellus viverra nulla ut metus varius laoreet. 
+                                        Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. 
+                                        Curabitur ullamcorper ultricies nisi.
                                     </Segment>
-
-                                </Sidebar.Pusher>
+                                </Segment>
                             </Grid.Row>
                             <br/>
                             <Grid.Row style={{marginRight: 40}}>
-                                <Sidebar.Pusher>
-                                    <Segment raised>
-                                        <Header as='h3'>Sprint 2</Header>
-                                        <Checkbox label='Feature 1' />
-                                        <Segment basic style={{textAlign: 'justify'}}>
-                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-                                            Aenean commodo ligula eget dolor. Aenean massa strong. 
-                                            Cum sociis natoque penatibus et magnis dis parturient montes, 
-                                            nascetur ridiculus mus. Donec quam felis, ultricies nec, 
-                                            pellentesque eu, pretium quis, sem. 
-                                            Nulla consequat massa quis enim. Donec pede justo, fringilla vel, 
-                                            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, 
-                                            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede 
-                                            link mollis pretium. Integer tincidunt. Cras dapibus. 
-                                            Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
-                                            Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. 
-                                            Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. 
-                                            Phasellus viverra nulla ut metus varius laoreet. 
-                                            Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. 
-                                            Curabitur ullamcorper ultricies nisi.
-                                        </Segment>
+                                <Segment raised>
+                                    <Header as='h3'>Sprint 2</Header>
+                                    <Checkbox label='Feature 1' />
+                                    <Segment basic style={{textAlign: 'justify'}}>
+                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+                                        Aenean commodo ligula eget dolor. Aenean massa strong. 
+                                        Cum sociis natoque penatibus et magnis dis parturient montes, 
+                                        nascetur ridiculus mus. Donec quam felis, ultricies nec, 
+                                        pellentesque eu, pretium quis, sem. 
+                                        Nulla consequat massa quis enim. Donec pede justo, fringilla vel, 
+                                        aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, 
+                                        imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede 
+                                        link mollis pretium. Integer tincidunt. Cras dapibus. 
+                                        Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
+                                        Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. 
+                                        Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. 
+                                        Phasellus viverra nulla ut metus varius laoreet. 
+                                        Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. 
+                                        Curabitur ullamcorper ultricies nisi.
                                     </Segment>
-                                </Sidebar.Pusher>
+                                </Segment>
                             </Grid.Row>
-
                         </Grid.Column>
                     </Grid.Row>
-
                 </Grid>
             </Sidebar.Pushable>
-
 
         );
     }
