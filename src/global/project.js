3e1798d59ff0426424273d43bbfb2473b606ccb5
ViniciusGarciaSilva <vinicius.garcia.silva@usp.br>
2018-10-19 09:24:02 -0300

    Global cliente

--- src/global/project.js
+++ src/global/project.js
@@ -4,15 +4,26 @@ export class Project {
   notFunctionalRequirements;
   sprints;
 
-  constructor() {
-    this.general = {
-      name: '',
-      theme: '',
-      details: '',
-    };
-    this.functionalRequirements = [];
-    this.notFunctionalRequirements = [];
-    this.sprints = [];
+  constructor(general, functionalRequirements, notFunctionalRequirements, sprints ) {
+    if(general===undefined) 
+      this.general = { name: '', theme: '', details: ''};
+    else
+      this.general = general;
+    
+    if(functionalRequirements===undefined)
+      this.functionalRequirements = [];
+    else
+      this.functionalRequirements = functionalRequirements;
+
+    if(notFunctionalRequirements===undefined)
+      this.notFunctionalRequirements = [];
+    else
+      this.notFunctionalRequirements = notFunctionalRequirements;
+
+    if(sprints===undefined)
+      this.sprints = [];
+    else
+      this.sprints = sprints;
   }
 }
 
